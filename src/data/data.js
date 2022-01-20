ade8f1355a06c59915b211b600645b659a82b7ab
bayogluteoman <bayogluteoman@gmail.com>
2022-01-20 16:37:13 +0300

    complete jsx lab

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,6 @@
 // use these variables in your JSX (update them with your own name and city!)
-const name = "Liza";
-const city = "New York";
+const name = "Teoman";
+const city = "Eskişehir";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
